bc8b334ffc84ec7ae6d4b64487bb6de2bd32b478
reckyy <srreki98@gmail.com>
2024-01-07 22:17:57 +0900

    ⚡️ feat: cluiを使えるところまで実装

--- countdown.js
+++ countdown.js
@@ -1,3 +1,20 @@
+import clui from 'clui';
+
+const Gauge = clui.Gauge;
+
 const bedtimeHour = process.argv[2];
 const thisTime = new Date();
 const hourLeft = parseInt(bedtimeHour) - thisTime.getHours();
+const percent = (hourLeft / 24) * 100;
+
+let sentence;
+
+if (percent <= 25){
+  sentence = "Have a nice day!";
+} else if(percent <= 50){
+  sentence = "Only half a day left! Let's do our best!"
+}else{
+  sentence = "You've done a great job, get ready to rest.";
+}
+
+console.log(Gauge(percent, 100, 20, 90, sentence));
